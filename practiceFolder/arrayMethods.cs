9cc57b1f7f73b7d2172b96cc93d26412a7b900f8
nick v <nvalente.electrics@gmail.com>
2024-02-08 15:46:05 +0000

    added switch statements and variables

--- practiceFolder/arrayMethods.cs
+++ practiceFolder/arrayMethods.cs
@@ -1,6 +1,42 @@
 @page
 @model IndexModel
 
+// Create a new Variable
+@{ 
+int myNumber = 33;
+string myName = "Nick"
+}
+
+// Switch Statements
+@{ string grade = "A"; }
+<div class="text-center">
+  <h1 class="display-5">Let's work with conditionals!</h1>
+  <hr>
+
+  <h3>Exam results:</h3>
+  <br>
+  <!--   Switch statement below -->
+  @switch (grade) {
+    case "A": <h4>Excellent job!</h4>
+    break;
+    case "B": <h4>Well done!</h4>
+    break;
+    case "C": <h4>Needs some work!</h4>
+    break;
+    case "D": <h4>“You passed”</h4>
+    break;
+    case "E": <h4>Excellent job!</h4>
+    break;
+    case "F": <h4>You failed, better try next time</h4>
+    break;
+    default: <h4>Invalid grade!</h4>;
+    break;
+  }
+  <h4>Your grade is, @grade</h4>
+</div>
+
+
+// Array methods
 <!-- Create favoriteFoods below -->
 @{
 List<string> favoriteFoods = new List<string>()
